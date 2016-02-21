9928429935d4d4601fa64e55b47e4508a19db65b
Nathanael Langlois <nathanael.langlois@gmail.com>
2016-02-21 10:05:39 +0100

    Added test file

---
+++ test/runtests.jl
@@ -0,0 +1,9 @@
+using Base.Test
+using Tlaloc
+
+# Testing constructor
+engine = TlalocEngine(string(dirname(Base.source_path()),"\\test_conf.ini"))
+@test typeof(engine) == TlalocEngine
+@test engine.viewPath == "thisIsTheViewPath"
+@test engine.TemplatePath == "thisIsTheTemplatePath"
+@test engine.ResourcePath == "thisIsTheResource"
