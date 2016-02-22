23c1e22a1dbb79df1f7c9b6763dd517ad15a8e30
Neabfi <fabien.delattre@outlook.com>
2016-02-22 12:43:58 +0100

    Change Backslash for slash ini path

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,15 +1,11 @@
 using Base.Test
 using Tlaloc
 
-#=
-
 # Testing constructor
-engine = TlalocEngine(string(dirname(Base.source_path()),"\\test_conf.ini"))
+engine = TlalocEngine(string(dirname(Base.source_path()),"/test_conf.ini"))
 @test typeof(engine) == TlalocEngine
 @test engine.viewPath == "thisIsTheViewPath"
 @test engine.TemplatePath == "thisIsTheTemplatePath"
 @test engine.ResourcePath == "thisIsTheResource"
-
-=#
 
 @test 1==1
