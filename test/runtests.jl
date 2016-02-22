892ae45fe9b89ab35d403610f44080a44f047c0e
Nathanael Langlois <nathanael.langlois@gmail.com>
2016-02-22 15:38:19 +0100

    Added a few tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,4 +8,12 @@ engine = TlalocEngine(string(dirname(Base.source_path()),"/test_conf.ini"))
 @test engine.TemplatePath == "thisIsTheTemplatePath"
 @test engine.ResourcePath == "thisIsTheResource"
 
-@test 1==1
+#Testing Page constructor
+aPage = Page(engine,"apage.html",Dict())
+@test typeof(aPage) == Page
+@test aPage.view == "apage.html"
+
+#Trying to add args
+addArg(aPage,"anArgument","aValue")
+@test haskey(aPage,"anArgument")
+@test (aPage.args)["anArgument"] == "aValue"
